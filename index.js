c742f8d910040e91f1653d3ff84dca16c5fa7534
A-Shushi <219606551+A-Shushi@users.noreply.github.com>
2025-09-16 14:00:15 +0700

    Display books in a table

--- index.js
+++ index.js
@@ -9,7 +9,30 @@ function Book(title, author, pages, readStatus, id) {
 }
 
 function addBookToLibrary(title, author, pages, readStatus) {
-    let uuid = crypto.randomUUID()
-    let book = new Book(title, author, pages, readStatus, uuid)
+    const uuid = crypto.randomUUID()
+    const book = new Book(title, author, pages, readStatus, uuid)
     myLibrary.push(book)
 }
+
+const tableHeader = document.getElementById("library-table")
+
+function displayBooks() {
+    for (const book of myLibrary) {
+        const newRow = document.createElement("tr")
+        for (const key in book) {
+            if (key !== "id") {
+                const tableElement = document.createElement('td')
+                tableElement.textContent = book[key]
+                newRow.appendChild(tableElement)
+            }
+        }
+        tableHeader.appendChild(newRow)
+    }
+}
+
+//Temporary books for setting up HTML and CSS
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 250, false)
+addBookToLibrary("Harry Potter", "J.K. Rowling", 201, true)
+
+displayBooks()
